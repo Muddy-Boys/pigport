9be146ead9a16ef454650b6ea0d073f56f32040d
dcti <mframe45@gmail.com>
2017-11-09 13:06:02 -0500

    merging

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,24 +1,42 @@
-// Get the modal
-var modal = document.getElementById('marc_modal');
-// Get the button that opens the modal
-var btn = document.getElementById("marc_btn");
+var modals = [];
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+modals = document.getElementsByClassName("modal");
+var open_btns = [];
+var close_btns = [];
+var ids = [];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+for(let i = 0; i < modals.length; i++){
+    ids.push(modals[i].id)
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+for(j in ids){
+    open_btns.push(document.getElementById(modals[j].id+"_btn"));
+    close_btns.push(document.querySelectorAll("#" + modals[j].id + " > div.modal-content > div.modal-header > span")[0]);
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// console.log('TEST')
+
+console.log(open_btns)
+console.log(close_btns)
+console.log(ids)
+
+for(let k = 0; k < ids.length; k++){
+    open_btns[k].onclick = function() {
+        modals[k].style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    close_btns[k].onclick = function() {
+        modals[k].style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+}
+
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    for(let k = 0; k < ids.length; k++){
+        if (event.target == modals[k]) {
+            modals[k].style.display = "none";
+        }
     }
 }
